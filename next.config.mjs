2af0084786454c64a28beddb168df9fea37ee0b6
deni-kurniawan <deni.kurniawan2@wingscorp.com>
2026-06-10 09:21:36 +0700

    update: configuration and layout adjustments

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // Comment/remove output: 'export' to enable dynamic API routes (Forms/Proxy) locally or on dynamic hosting.
-  output: 'export',
+  // output: 'export',
   images: {
     unoptimized: true,
   },
   productionBrowserSourceMaps: true,
+  experimental: {
+    inlineCss: true,
+  },
 };
 export default nextConfig;
